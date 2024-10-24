6e72c5f1578d5fce53baf6d3e829ee0795c75065
Jing Huang <jinghuang@Jings-MacBook-Air.local>
2024-10-24 13:52:40 -0400

    Finished implementation of Workouts page.

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { DAYSOFWEEK } from "../constants/DaysOfWeek";
+import React, { useState } from 'react'
+import { DAYSOFWEEK } from '../constants/DaysOfWeek'
 import {
   TextField,
   Button,
@@ -13,89 +13,79 @@ import {
   DialogTitle,
   FormControl,
   InputLabel,
-  MenuItem,
-} from "@mui/material";
+  MenuItem
+} from '@mui/material'
 
-const WorkoutForm = (props) => {
-  // Initial state for the workouts = [{{daysofweek: {id, workoutTitle, duration, startTime, description(exercises : [] ), link }}}]
-  // TODO: don't do it Initial state for exercises = [{exerciseTitle: "", startTime: "", link: "", reps: "", set: ""}]
+const WorkoutForm = props => {
   const [workoutData, setWorkoutData] = useState({
-    selectedDay: "",
-    workoutTitle: "",
-    description: "",
-    duration: "",
-    link: "",
+    selectedDay: '',
+    workoutTitle: '',
+    description: '',
+    duration: '',
+    link: ''
   });
 
-  const [errors, setErrors] = useState({});
-
-  console.log("workoutData: ", workoutData);
+  const [errors, setErrors] = useState({})
 
   // Handle input changes
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setWorkoutData({ ...workoutData, [name]: value });
-    setErrors({ ...errors, [name]: "" }); // Clear error when user starts typing
-  };
-
-  // const handleTaskChange = (day, index) => (event) => {
-  //   const { value, name } = event.target;
-  //   const updatedTasks = [...workouts[day].tasks];
-  //   if (!updatedTasks[index]) {
-  //     updatedTasks[index] = {};
-  //   }
-  //   updatedTasks[index] = { ...updatedTasks[index], [name]: value };
-  //   setWorkouts((prev) => ({
-  //     ...prev,
-  //     [day]: { ...prev[day], tasks: updatedTasks },
-  //   }));
-  // };
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    setWorkoutData({ ...workoutData, [name]: value })
+    setErrors({ ...errors, [name]: '' }) // Clear error when user starts typing
+  }
 
   // Handle form submission
 
-  const isValidURL = (url) => {
-    const urlPattern = /^(https?:\/\/)?([a-z0-9-]+\.)+[a-z]{2,6}(\/[^\s]*)?$/i;
-    return urlPattern.test(url);
-  };
+  const isValidURL = url => {
+    const urlPattern = /^(https?:\/\/)?([a-z0-9-]+\.)+[a-z]{2,6}(\/[^\s]*)?$/i
+    return urlPattern.test(url)
+  }
 
   // Handle submit clicks
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault()
 
-    const newErrors = {};
+    const newErrors = {}
 
     // Validation
     if (!workoutData.selectedDay) {
-      newErrors.selectedDay = "Please select a day.";
+      newErrors.selectedDay = 'Please select a day.'
     }
     if (!workoutData.workoutTitle) {
-      newErrors.workoutTitle = "Workout title is required.";
+      newErrors.workoutTitle = 'Workout title is required.'
     }
     if (!workoutData.duration || workoutData.duration <= 0) {
-      newErrors.duration = "Please enter a valid duration in minutes.";
+      newErrors.duration = 'Please enter a valid duration in minutes.'
     }
     if (workoutData.link && !isValidURL(workoutData.link)) {
-      newErrors.link = "Please enter a valid URL.";
+      newErrors.link = 'Please enter a valid URL.'
     }
 
-    setErrors(newErrors);
+    // Check for duplicate workout title
+    const existingSchedules = props.schedules || [] // Default to an empty array
+    const isDuplicateTitle = existingSchedules.some(
+      workout => workout.workoutTitle === workoutData.workoutTitle
+    )
+    if (isDuplicateTitle) {
+      newErrors.workoutTitle = 'Workout title must be unique.'
+    }
 
+    setErrors(newErrors)
     if (Object.keys(newErrors).length === 0) {
-      // Handle successful form submission logic here, e.g., send data to an API
-      console.log("Submitted Workouts:", workoutData);
-      handleClose(); // Close the modal after submission
+      props.onChange(workoutData.selectedDay, workoutData)
+      handleClose() // Close the modal after submission
     }
-  };
+  }
 
   const handleClose = () => {
-    props.setAddMode(false);
-  };
+    props.setAddMode(false)
+  }
 
   // Reusable text field function
   const renderTextField = (
     name,
     label,
-    type = "text",
+    type = 'text',
     multiline = false,
     rows = 1
   ) => (
@@ -106,29 +96,29 @@ const WorkoutForm = (props) => {
       type={type}
       value={workoutData[name]}
       onChange={handleInputChange}
-      sx={{ marginTop: "20px" }}
+      sx={{ marginTop: '20px' }}
       multiline={multiline}
       rows={rows}
       error={!!errors[name]}
       helperText={errors[name]}
     />
-  );
+  )
 
   return (
-    <Dialog open={props.addMode} onClose={handleClose} fullWidth maxWidth="md">
+    <Dialog open={props.addMode} onClose={handleClose} fullWidth maxWidth='md'>
       <DialogTitle>New Workout Form</DialogTitle>
       <DialogContent>
-        <FormControl fullWidth sx={{ marginTop: "10px" }}>
-          <InputLabel id="day-in-week-label">Select Day</InputLabel>
+        <FormControl fullWidth sx={{ marginTop: '10px' }}>
+          <InputLabel id='day-in-week-label'>Select Day</InputLabel>
           <Select
-            labelId="day-in-week-label"
-            id="day-in-week-select"
+            labelId='day-in-week-label'
+            id='day-in-week-select'
             value={workoutData.selectedDay}
             onChange={handleInputChange}
-            label="Select Day"
-            name="selectedDay"
+            label='Select Day'
+            name='selectedDay'
           >
-            {DAYSOFWEEK.map((day) => (
+            {DAYSOFWEEK.map(day => (
               <MenuItem key={day.value} value={day.value}>
                 {day.label}
               </MenuItem>
@@ -136,23 +126,23 @@ const WorkoutForm = (props) => {
           </Select>
         </FormControl>
         {errors.selectedDay && (
-          <span style={{ color: "red" }}>{errors.selectedDay}</span>
+          <span style={{ color: 'red' }}>{errors.selectedDay}</span>
         )}
-        {renderTextField("workoutTitle", "Workout Title")}
-        {renderTextField("duration", "Duration (minutes)", "number")}
-        {renderTextField("link", "Video Link")}
-        {renderTextField("description", "Description", "text", true, 4)}
+        {renderTextField('workoutTitle', 'Workout Title')}
+        {renderTextField('duration', 'Duration (minutes)', 'number')}
+        {renderTextField('link', 'Video Link')}
+        {renderTextField('description', 'Description', 'text', true, 4)}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button onClick={handleClose} color='primary'>
           Cancel
         </Button>
-        <Button type="submit" color="primary" onClick={handleSubmit}>
+        <Button type='submit' color='primary' onClick={handleSubmit}>
           Submit
         </Button>
       </DialogActions>
     </Dialog>
-  );
-};
+  )
+}
 
-export default WorkoutForm;
+export default WorkoutForm
--- frontend/src/components/Workouts.js
+++ frontend/src/components/Workouts.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {
   Container,
   Typography,
@@ -7,121 +7,185 @@ import {
   Divider,
   Box,
   Tab,
-  Tabs,
-} from "@mui/material";
-import Footer from "./Footer";
-import WorkoutDisplay from "./WorkoutDisplay";
-import WorkoutForm from "./WorkoutForm";
-import { DAYSOFWEEK } from "../constants/DaysOfWeek";
+  Tabs
+} from '@mui/material'
+import Footer from './Footer'
+import WorkoutDisplay from './WorkoutDisplay'
+import WorkoutForm from './WorkoutForm'
+import { DAYSOFWEEK, workoutTitles } from '../constants/DaysOfWeek'
+import headerImage from '../images/header.webp'
 
 const commonContainerStyles = {
-  width: "100%",
-  display: "flex",
-  justifyContent: "center", // Center horizontally
-  alignItems: "center", // Center vertically
-};
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center', // Center horizontally
+  alignItems: 'center' // Center vertically
+}
 
 const commonBoxStyles = {
-  border: "1px solid gray",
-  borderRadius: "4px",
-  boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
-  padding: "16px",
-  margin: "16px",
-  width: "100%",
-};
+  border: '1px solid gray',
+  borderRadius: '4px',
+  boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
+  padding: '16px',
+  margin: '16px',
+  width: '100%'
+}
 
-const Workouts = (props) => {
-  const [day, setDay] = useState("Monday");
-  const [editMode, setEditMode] = useState(false);
-  const [addMode, setAddMode] = useState(false);
-  const [schedules, setSchedules] = useState([]);
+const Workouts = props => {
+  const [day, setDay] = useState('Monday')
+  const [editMode, setEditMode] = useState(false)
+  const [addMode, setAddMode] = useState(false)
+  const [schedules, setSchedules] = useState({
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+    Saturday: [],
+    Sunday: []
+  })
 
-  const handleCreateSchedule = (wk) => {
-    axios({
-      method: "post",
-      url: "/createSchedule",
-      headers: {
-        Authorization: "Bearer " + props.state.token,
-      },
-      data: {
-        weekSchedule: wk,
-      },
+  const handleAddSchedule = (selectedDay, workoutData) => {
+    setSchedules(prevSchedules => {
+      const updatedSchedules = {
+        ...prevSchedules,
+        [selectedDay]: [...(prevSchedules[selectedDay] || []), workoutData]
+      }
+
+      axios
+        .post(
+          '/createSchedule',
+          {
+            weekSchedule: updatedSchedules
+          },
+          {
+            headers: {
+              Authorization: 'Bearer ' + props.state.token
+            }
+          }
+        )
+        .then(response => console.log(response.data))
+        .catch(error =>
+          console.error('Error:', error.response || error.message)
+        )
+
+      return updatedSchedules
     })
-      .then((response) => {
-        const res = response.data;
-        console.log(res);
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-  };
+  }
 
-  // handleCreateSchedule(WS);
-  console.log("checking schedule", schedules);
+  const handleRemoveSchedule = (selectedDay, workoutTitle) => {
+    setSchedules(prevSchedules => {
+      const updatedSchedules = {
+        ...prevSchedules,
+        [selectedDay]: prevSchedules[selectedDay].filter(
+          workout => workout.workoutTitle !== workoutTitle
+        )
+      }
+
+      axios
+        .delete(`/deleteSchedule/${selectedDay}/${workoutTitle}`, {
+          headers: {
+            Authorization: 'Bearer ' + props.state.token
+          }
+        })
+        .then(response => {
+          console.log('Schedule updated successfully:', response.data)
+        })
+        .catch(error =>
+          console.error('Error:', error.response || error.message)
+        )
+
+      return updatedSchedules
+    })
+  }
+
   useEffect(() => {
-    axios
-      .get("/mySchedule", {
-        headers: {
-          Authorization: "Bearer " + props.state.token, // Ensure the token is valid
-        },
-      })
-      .then((response) => {
-        console.log(response.data.data);
-        setSchedules(response.data.data);
-      })
-      .catch((error) => {
+    const fetchSchedules = async () => {
+      try {
+        const response = await axios.get('/mySchedule', {
+          headers: {
+            Authorization: 'Bearer ' + props.state.token
+          }
+        })
+        setSchedules(response.data.data)
+      } catch (error) {
         console.error(
-          "Error fetching schedule:",
+          'Error fetching schedule:',
           error.response || error.message
-        );
-      });
-  }, []);
+        )
+      }
+    }
+    fetchSchedules()
+  }, [props.state.token])
 
   return (
-    <Container maxWidth={false} sx={{ width: "90%" }}>
-      <WorkOutHeader />
-      <Container>
-        <Box
-          sx={{
-            justifyContent: "space-between",
-            display: "flex",
-          }}
-        >
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            onClick={() => {
-              setAddMode(!addMode);
+    <Container maxWidth={false} sx={{ width: '90%' }}>
+      <Box
+        sx={{
+          marginTop: '10px',
+          backgroundImage: `url(${headerImage})`,
+          backgroundSize: '55%',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          padding: '100px',
+          position: 'relative',
+          overflow: 'hidden'
+        }}
+      >
+        <WorkOutHeader />
+        <Container>
+          <Box
+            sx={{
+              justifyContent: 'space-between',
+              display: 'flex'
             }}
-            style={{ backgroundColor: "orange", color: "white" }}
           >
-            Add
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            onClick={() => {
-              setEditMode(!editMode);
-            }}
-            style={{ backgroundColor: "orange", color: "white" }}
-          >
-            Edit
-          </Button>
-        </Box>
-      </Container>
+            <Button
+              type='submit'
+              variant='contained'
+              size='small'
+              onClick={() => {
+                setAddMode(!addMode)
+              }}
+              style={{
+                backgroundColor: 'orange',
+                color: 'white',
+                width: '100px'
+              }}
+            >
+              Add
+            </Button>
+            <Button
+              type='submit'
+              variant='contained'
+              size='large'
+              onClick={() => {
+                setEditMode(!editMode)
+              }}
+              style={{
+                backgroundColor: 'orange',
+                color: 'white',
+                width: '100px'
+              }}
+            >
+              Edit
+            </Button>
+          </Box>
+        </Container>
+      </Box>
       <Container>
-        {/* TODO: this is for pop out the new components */}
-        {addMode && <WorkoutForm addMode={addMode} setAddMode={setAddMode} />}
+        {addMode && (
+          <WorkoutForm
+            addMode={addMode}
+            setAddMode={setAddMode}
+            onChange={handleAddSchedule}
+            schedules={schedules[day]}
+          />
+        )}
       </Container>
       <Container
         sx={{
-          ...commonContainerStyles,
+          ...commonContainerStyles
         }}
         disableGutters
         maxWidth={false}
@@ -130,246 +194,57 @@ const Workouts = (props) => {
           <Tabs
             value={day}
             onChange={(e, newValue) => {
-              setDay(newValue);
+              setDay(newValue)
             }}
             TabIndicatorProps={{
-              sx: { backgroundColor: "#FFA000" },
+              sx: { backgroundColor: '#FFA000' }
             }}
             sx={{
-              width: "100%",
-              "& .MuiTab-root": {
-                color: "black",
-                "&.Mui-selected": {
-                  color: "#FFA000",
-                },
-              },
+              width: '100%',
+              '& .MuiTab-root': {
+                color: 'black',
+                '&.Mui-selected': {
+                  color: '#FFA000'
+                }
+              }
             }}
             centered
-            variant="fullWidth"
+            variant='fullWidth'
           >
-            {DAYSOFWEEK.map((day) => (
+            {DAYSOFWEEK.map(day => (
               <Tab key={day.value} label={day.label} value={day.value} />
             ))}
           </Tabs>
-          <Divider sx={{ width: "100%", margin: "8px 0" }} />
-          <WorkoutDisplay schedule={schedules[day]} />
+          <Divider sx={{ width: '100%', margin: '8px 0' }} />
+          {schedules && day && (
+            <WorkoutDisplay
+              schedules={schedules[day]}
+              editMode={editMode}
+              handleRemove={handleRemoveSchedule}
+            />
+          )}
         </Box>
       </Container>
       <Footer />
     </Container>
-  );
-};
-
-const WS = {
-  Monday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 1,
-        title: "Strength Workout 1",
-        CreateDate: "2024-10-10T10:00:00Z",
-        description: "Focus on compound movements.",
-        duration: 60,
-        tasks: [
-          {
-            exerciseTitle: "Bench Press",
-            videoLink: "http://example.com/strength-workout1-video",
-            exercises: [
-              {
-                reps: 10,
-                sets: 3,
-              },
-            ],
-          },
-          {
-            exerciseTitle: "Deadlift",
-            videoLink: "http://example.com/strength-workout1-video",
-            exercises: [
-              {
-                reps: 8,
-                sets: 3,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  Tuesday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 2,
-        title: "Cardio Workout",
-        CreateDate: "2024-10-10T11:00:00Z",
-        description: "Aim for a steady pace during cardio.",
-        duration: 60,
-        tasks: [
-          {
-            exerciseTitle: "Running",
-            videoLink: "http://example.com/cardio-workout-video",
-            exercises: [
-              {
-                durationInMinutes: 30,
-                intensity: "Moderate",
-              },
-            ],
-          },
-          {
-            exerciseTitle: "Cycling",
-            videoLink: "http://example.com/cardio-workout-video",
-            exercises: [
-              {
-                durationInMinutes: 30,
-                intensity: "High",
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  Wednesday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 3,
-        title: "Rest Day",
-        CreateDate: "2024-10-10T12:00:00Z",
-        description: "Take a day off for recovery.",
-        duration: 0,
-        tasks: [],
-      },
-    ],
-  },
-  Thursday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 4,
-        title: "Strength Workout 2",
-        CreateDate: "2024-10-10T13:00:00Z",
-        description: "Incorporate free weights for better strength.",
-        duration: 60,
-        tasks: [
-          {
-            exerciseTitle: "Squats",
-            videoLink: "http://example.com/strength-workout2-video",
-            exercises: [
-              {
-                reps: 12,
-                sets: 4,
-              },
-            ],
-          },
-          {
-            exerciseTitle: "Pull-ups",
-            videoLink: "http://example.com/strength-workout2-video",
-            exercises: [
-              {
-                reps: 8,
-                sets: 3,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  Friday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 5,
-        title: "Flexibility and Core",
-        CreateDate: "2024-10-10T14:00:00Z",
-        description: "Focus on flexibility and core stability.",
-        duration: 40,
-        tasks: [
-          {
-            exerciseTitle: "Yoga",
-            videoLink: "http://example.com/flexibility-core-video",
-            exercises: [
-              {
-                durationInMinutes: 30,
-              },
-            ],
-          },
-          {
-            exerciseTitle: "Plank",
-            videoLink: "http://example.com/flexibility-core-video",
-            exercises: [
-              {
-                durationInMinutes: 10,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  Saturday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 6,
-        title: "Outdoor Activity",
-        CreateDate: "2024-10-10T15:00:00Z",
-        description: "Enjoy the outdoors and stay active.",
-        duration: 120,
-        tasks: [
-          {
-            exerciseTitle: "Hiking",
-            videoLink: null,
-            exercises: [
-              {
-                durationInMinutes: 120,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  Sunday: {
-    date: "2024-10-10",
-    workouts: [
-      {
-        id: 7,
-        title: "Active Recovery",
-        CreateDate: "2024-10-10T16:00:00Z",
-        description: "Engage in light activity to aid recovery.",
-        duration: 30,
-        tasks: [
-          {
-            exerciseTitle: "Walking",
-            videoLink: null,
-            exercises: [
-              {
-                durationInMinutes: 30,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-};
+  )
+}
 
 const WorkOutHeader = () => {
   return (
-    <Container maxWidth="sm" style={{ textAlign: "center", marginTop: "60px" }}>
+    <Container maxWidth='sm' style={{ textAlign: 'center', marginTop: '20px' }}>
       <Typography
-        component="h1"
-        variant="h2"
-        align="center"
-        color="text.primary"
+        component='h1'
+        variant='h2'
+        align='center'
+        color='text.primary'
         gutterBottom
+        sx={{ color: 'Black' }}
       >
-        My Workout Week
+        <strong>My Workout Week</strong>
       </Typography>
     </Container>
-  );
-};
+  )
+}
 
-export default Workouts;
+export default Workouts
